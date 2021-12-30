15738cfa55f920803e9ad1c9a9d9c30b2c80052e
Joey Paul Eli Haynes <joeypaulhaynes@yahoo.com>
2021-12-29 18:45:26 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+// import Body from './components/Body';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyASbVGnoqXjQljVjgy44uE4RpPAuPEWJQg",
+  authDomain: "jpthefishdemo1.firebaseapp.com",
+  projectId: "jpthefishdemo1",
+  storageBucket: "jpthefishdemo1.appspot.com",
+  messagingSenderId: "57659623095",
+  appId: "1:57659623095:web:1974a271dabb5191616f32"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      {/* <Body /> */}
+      <section className="antique">
+        { user ? <ChatRoom /> : <SignIn /> }
+      </section>
+      <Footer />
+    </>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <button onClick={ signInWithGoogle }>Sign in with Google</button>
+  )
+}
+
+function ChatRoom() {
+  
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,117 @@
+const Navigation = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                <li className="logo">
+                    <a href="index.html" className="nav-link">
+                        <span className="link-text logo-text">jpthefish</span>
+                        <svg 
+                        aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="fish" 
+                        className="svg-inline--fa fa-fish fa-w-18" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 576 512"
+                        >
+                        <path fill="currentColor" d="M327.1 96c-89.97 0-168.54 54.77-212.27 101.63L27.5 131.58c-12.13-9.18-30.24.6-27.14 14.66L24.54 256 .35 365.77c-3.1 14.06 15.01 23.83 27.14 14.66l87.33-66.05C158.55 361.23 237.13 416 327.1 416 464.56 416 576 288 576 256S464.56 96 327.1 96zm87.43 184c-13.25 0-24-10.75-24-24 0-13.26 10.75-24 24-24 13.26 0 24 10.74 24 24 0 13.25-10.75 24-24 24z"></path>
+                        </svg>
+                    </a>
+                </li>
+            
+                <li className="nav-item">
+                    <a href="piano.html" className="nav-link">
+                    <svg 
+                        aria-hidden="true"
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="music" 
+                        className="svg-inline--fa fa-music fa-w-16" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 512 512"
+                    >
+                        <path fill="currentColor" d="M470.38 1.51L150.41 96A32 32 0 0 0 128 126.51v261.41A139 139 0 0 0 96 384c-53 0-96 28.66-96 64s43 64 96 64 96-28.66 96-64V214.32l256-75v184.61a138.4 138.4 0 0 0-32-3.93c-53 0-96 28.66-96 64s43 64 96 64 96-28.65 96-64V32a32 32 0 0 0-41.62-30.49z"></path>
+                    </svg>
+                    <span className="link-text">Play a Piano</span>
+                    </a>
+                </li>
+            
+                <li className="nav-item">
+                    <a href="messaging.html" className="nav-link">
+                    <svg 
+                        aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="comments" 
+                        className="svg-inline--fa fa-comments fa-w-18" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 576 512"
+                    >
+                        <path fill="currentColor" d="M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z"></path>
+                    </svg>
+                    <span className="link-text">Post a Message</span>
+                    </a>
+                </li>
+            
+                <li className="nav-item">
+                    <a href="feedback.html" className="nav-link">
+                    <svg 
+                        aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="lightbulb" 
+                        className="svg-inline--fa fa-lightbulb fa-w-11" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 352 512"
+                    >
+                        <path fill="currentColor" d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path>
+                    </svg>
+                    <span className="link-text">Share Feedback</span>
+                    </a>
+                </li>
+            
+                <li className="nav-item">
+                    <a href="about.html" className="nav-link">
+                    <svg 
+                        aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="address-card" 
+                        className="svg-inline--fa fa-address-card fa-w-18" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 576 512"
+                    >
+                        <path fill="currentColor" d="M528 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-352 96c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H86.4C74 384 64 375.4 64 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2zM512 312c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16zm0-64c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16zm0-64c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16z"></path>
+                    </svg>
+                    <span className="link-text">About the Developer</span>
+                    </a>
+                </li>
+            
+                <li className="nav-item">
+                    <a href="#" className="nav-link">
+                    <svg 
+                        aria-hidden="true" 
+                        focusable="false" 
+                        data-prefix="fas" 
+                        data-icon="globe" 
+                        className="svg-inline--fa fa-globe fa-w-16" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 496 512"
+                    >
+                        <path fill="currentColor" d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"></path>
+                    </svg>
+                    <span className="link-text">English ...</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
