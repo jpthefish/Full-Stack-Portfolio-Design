b0428d1fb08d95a96022401d2b6f3bd0288afd26
Joey Paul Eli Haynes <joeypaulhaynes@yahoo.com>
2022-01-06 13:29:55 -0600

    edit landing

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -2,7 +2,8 @@ const Piano = () => {
     return (
         <main>
             <section className="antique">
-
+                <h1>Currently in development</h1>
+                <p>Check back soon!</p>
             </section>
         </main>
     )
