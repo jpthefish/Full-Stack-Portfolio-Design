47f1e603e9842c2e9a8f9a30c2c64562de8d2f7b
Joey Paul Eli Haynes <joeypaulelihaynes@Joses-MacBook-Air.local>
2022-08-27 18:05:52 -0500

    resume

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ const About = () => {
             </section>
             <section className="cream">
                 <h1>My Experience</h1>
-                <p>In addition to this platform, other projects hosted on GitHub include an interactive sorting algorithm visualizer as well as website templates I've developed for businesses. I have design and development experience in several languages/frameworks, especially Python, SQL, and Javascript (with React.js).</p>
+                <p>In addition to this platform, other projects hosted on GitHub include an interactive sorting algorithm visualizer as well as website templates and data visualizations I've developed for businesses. I have design and development experience in several languages/frameworks, especially Python, SQL, and Javascript (with React.js).</p>
                 <div className="wave">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
