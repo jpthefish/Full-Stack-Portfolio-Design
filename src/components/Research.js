46ff2ad8f214e7aa7b2028913bfa22dd2c43034e
J.P. Haynes <joeypaulhaynes@yahoo.com>
2023-11-09 23:44:35 -0600

    update landing page text

--- src/components/Research.js
+++ src/components/Research.js
@@ -18,7 +18,8 @@ const Research = () => {
             <section className="antique">
                 <h1>Undergraduate Capstone</h1>
                 <p>Haynes, J.P. (2022) <i>"Global Society and Contemporary Immigration to Western Europe: Effects on Identity, Demographics, and Cultural Expression"</i></p>
-                <ul><li><a href="/Undergraduate Capstone.pdf" target="_blank" rel="noopener noreferrer"><button className="button">View PDF</button></a></li></ul>
+                {/* Normally "Undergraduate Capstone.pdf" would be here */}
+                <ul><li><a href="/Global Development & Demography Paper.pdf" target="_blank" rel="noopener noreferrer"><button className="button">View PDF</button></a></li></ul>
                 <div className="wave">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
