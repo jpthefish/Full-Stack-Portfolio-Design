c863cf460fed589315ad10544d11026a98db4d75
J.P. Haynes <joeypaulhaynes@yahoo.com>
2024-11-15 02:49:50 -0600

    updated resume and UI buttons

--- src/components/Research.js
+++ src/components/Research.js
@@ -3,13 +3,53 @@ const Research = () => {
         <main>
             <section className="antique">
                 <h1>Research Projects</h1>
-                <p>For full list of publications, please visit my Google Scholar Page:</p>
-                <ul><li><a href="https://scholar.google.com/schhp?hl=en" target="_blank" rel="noopener noreferrer"><button className="button">Google Scholar</button></a></li></ul>
+                <p>For more publications, please visit my Google Scholar Page:</p>
+                <ul>
+                    <li>
+                        <a href="https://scholar.google.com/citations?user=Vzh0FpYAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">
+                            <button className="button">Google Scholar</button>
+                        </a>
+                    </li>
+                </ul>
             </section>
             <section className="cream">
                 <h1>NSF/ASME Poster, IDETC-CIE 2024</h1>
                 <p><i>"Adaptive Manufacturing Systems: Leveraging Predictive Modeling and Cyber-Physical-Social Systems (CPSS) for Real-Time Adaptation"</i></p>
-                <ul><li><a href="/Design Essay IDETC Poster.pdf" target="_blank" rel="noopener noreferrer"><button className="button">View PDF</button></a></li></ul>
+                <ul>
+                    <li>
+                        <a href="/Design Essay IDETC Poster.pdf" target="_blank" rel="noopener noreferrer">
+                            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="poster" class="svg-inline--fa fa-github-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path fill="currentColor" d="M448 80c8.8 0 16 7.2 16 16l0 319.8-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3L48 96c0-8.8 7.2-16 16-16l384 0zM64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a href="/NSF ASME Student Design Essay.pdf" target="_blank" rel="noopener noreferrer">
+                            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="paper" class="svg-inline--fa fa-github-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path fill="currentColor" d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z"/>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+                <div className="wave">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+                    </svg>
+                </div>
+            </section>
+            <section className="antique">
+                <h1>Undergraduate Capstone</h1>
+                <p><i>"Contemporary Economic Migration: Impacts on Global Wealth, Demographics, and Expression"</i></p>
+                <ul>
+                    <li>
+                        <a href="/Undergraduate Capstone.pdf" target="_blank" rel="noopener noreferrer">
+                            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="paper" class="svg-inline--fa fa-github-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path fill="currentColor" d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z"/>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
                 <div className="wave">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
